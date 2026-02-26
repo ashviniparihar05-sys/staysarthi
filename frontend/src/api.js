84ce39fe28f1f5b19bd2c5d14aa3f7a96a71da6c
ashviniparihar05-sys <ashvini.parihar05@gmail.com>
2026-02-26 17:45:46 +0530

    Switch API endpoint to production URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 // const API = "https://staysarthi-production.up.railway.app";
 
 // export default API;
-//const API = "https://staysarthi-5.onrender.com";
-const API = "http://localhost:8080";
+const API = "https://staysarthi-5.onrender.com";
+//const API = "http://localhost:8080";
 
 export default API;
